1f5bac7965d03867527a549e31889bc5b2f4fc8a
Micah Pennington <mpennin3@hotmail.com>
2022-01-29 23:26:57 -0500

    Final Build. Gave the app an icon and name. Also added constraints for support of other iPhone models

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -88,6 +88,11 @@ class ViewController: UIViewController {
         score = 0
         round = 0
         startNewRound()
+        let transition = CATransition()
+        transition.type = CATransitionType.fade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        view.layer.add(transition, forKey: nil)
     }
     
     func startNewRound() {
